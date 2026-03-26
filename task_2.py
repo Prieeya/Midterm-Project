615a8cb19501723ba134e71b17cda834be0a5ed9
Pramisha Pathak <pramisha1436@gmail.com>
2026-03-26 12:38:17 -0600

    codes

--- task_2.py
+++ task_2.py
@@ -10,7 +10,7 @@ X_test  = test_images.reshape(len(test_images), -1).astype("float64")
 Xf_train = f_train_images.reshape(len(f_train_images), -1).astype("float64")
 Xf_test  = f_test_images.reshape(len(f_test_images), -1).astype("float64")
 
-print("After Flattening & Standardization:")
+print("After Flattening:")
 print("MNIST Train:", X_train.shape)
 print("MNIST Test:", X_test.shape)
 print("Fashion MNIST Train:", Xf_train.shape)
